b91026beb468288f20b9c62f53474cc184b35a62
bdew <blackdew@gmail.com>
2019-12-06 17:05:21 +0200

    added lockSeason setting

--- src/main/java/net/bdew/wurm/nowinter/NoWinterMod.java
+++ src/main/java/net/bdew/wurm/nowinter/NoWinterMod.java
@@ -1,18 +1,27 @@
 package net.bdew.wurm.nowinter;
 
+import com.wurmonline.client.game.SeasonManager;
 import org.gotti.wurmunlimited.modloader.classhooks.HookManager;
 import org.gotti.wurmunlimited.modloader.classhooks.InvocationHandlerFactory;
+import org.gotti.wurmunlimited.modloader.interfaces.Configurable;
 import org.gotti.wurmunlimited.modloader.interfaces.Initable;
 import org.gotti.wurmunlimited.modloader.interfaces.PreInitable;
-import org.gotti.wurmunlimited.modloader.interfaces.WurmMod;
+import org.gotti.wurmunlimited.modloader.interfaces.WurmClientMod;
 
 import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Method;
+import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class NoWinterMod implements WurmMod, Initable, PreInitable {
+public class NoWinterMod implements WurmClientMod, Initable, PreInitable, Configurable {
     private static Logger logger = Logger.getLogger("NoWinterMod");
+
+    private static int lockSeason = -1;
+
+    @Override
+    public void configure(Properties properties) {
+        lockSeason = Integer.parseInt(properties.getProperty("lockSeason", "-1"));
+    }
 
     @Override
     public void init() {
@@ -21,9 +30,10 @@ public class NoWinterMod implements WurmMod, Initable, PreInitable {
             HookManager.getInstance().registerHook("com.wurmonline.client.game.SeasonManager", "selectSeason", "(FF)Lcom/wurmonline/client/game/SeasonManager$Season;", new InvocationHandlerFactory() {
                 @Override
                 public InvocationHandler createInvocationHandler() {
-                    return new InvocationHandler() {
-                        @Override
-                        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                    return (proxy, method, args) -> {
+                        if (lockSeason >= 0) {
+                            return SeasonManager.Season.values()[lockSeason];
+                        } else {
                             if ((Float) args[0] < 0)
                                 args[0] = 0f;
                             return method.invoke(proxy, args);
